3176dbdf13ad26e83654640c5b5ce1072a0f6323
W∆L13R <76449145+ritwik-ritnov@users.noreply.github.com>
2021-12-10 04:53:06 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -190,11 +190,11 @@ async def yt_dlp_call_back(bot, update):
         time_taken_for_download = (end_one -start).seconds
         file_size = Config.TG_MAX_FILE_SIZE + 1
         try:
-            file_size = os.stat(download_directory).st_size
+            file_size = os.stat(yfile).st_size
         except FileNotFoundError as exc:
             download_directory = os.path.splitext(download_directory)[0] + "." + "mkv"
             # https://stackoverflow.com/a/678242/4723940
-            file_size = os.stat(download_directory).st_size
+            file_size = os.stat(yfile).st_size
         if file_size > Config.TG_MAX_FILE_SIZE:
             await bot.edit_message_text(
                 chat_id=update.message.chat.id,
