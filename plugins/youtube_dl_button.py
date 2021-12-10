a23343ee8a2d5afaf494d12a74cd1bb5277ddfcf
W∆L13R <76449145+ritwik-ritnov@users.noreply.github.com>
2021-12-10 12:08:58 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -155,7 +155,7 @@ async def yt_dlp_call_back(bot, update):
     if yt_dlp_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(yt_dlp_password)
-    if "hoichoi" in url:
+    if "hoichoi" in yt_dlp_url:
         command_to_exec.append("--cookies")
         command_to_exec.append("./cookies.txt")
     command_to_exec.append("--no-warnings")
